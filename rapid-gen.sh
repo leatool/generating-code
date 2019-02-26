722442a9afe0061ad9f9b59b1351c373c2f0a264
shijinyuan <13152720003@163.com>
2019-02-26 14:58:34 +0800

    JAVA code generator

---
+++ rapid-gen.sh
@@ -0,0 +1,6 @@
+echo 1. 生成器自带了测试数据库,运行start_db.bat后再运行gen user_info即可生成文件
+echo 2. 生成器的主要配置文件为generator.xml,里面修改数据库连接属性
+echo 3. template目录为代码生成器的模板目录,可自由调整模板的目录结构
+export classpath=$CALSSPATH:./lib/*:./lib/rapid-generator.jar:./lib/freemarker.jar:./lib/h2-1.2.137.jar:./lib/log4j-1.2.15.jar:./lib/mysql-connector-java-5.0.5-bin.jar:./lib/ojdbc14.jar:./lib/postgresql-8.4-701.jdbc3.jar:./lib/sqljdbc.jar
+export PATH=$JAVA_HOME/bin:$PATH
+java  -classpath $classpath -server -Xms128m -Xmx384m cn.org.rapid_framework.generator.ext.CommandLine -DtemplateRootDir=template -Dfile.encoding=gb2312
